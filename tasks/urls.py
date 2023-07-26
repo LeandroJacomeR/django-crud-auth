05a14fe4b0b84a11dacc6591708df4cafae1e0a9
Leandro Jacome <ljacomera@ufpso.edu.co>
2023-07-26 00:06:04 -0500

    Django CRUD

---
+++ tasks/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from tasks import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('tasks/', views.tasks, name='tasks'),
+    path('logout/', views.signout, name='logout'),
+    path('signin/', views.signin, name='signin'),
+]
